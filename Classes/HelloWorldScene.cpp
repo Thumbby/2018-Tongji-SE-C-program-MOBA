b9c3cab31b7eb20eb2664dde87fce32cc2e88ea4
yanyu <49723522+Yangyu0879@users.noreply.github.com>
2019-05-19 12:39:26 +0800

    Add files via upload

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -33,13 +33,13 @@ bool HelloWorld::init()
 	image = new Image();
 	image->initWithImageFile("gf.png");
 	CCLOG("%d，%d", image->getHeight(), image->getWidth());
-	
+
 	background = Sprite::create("background2.jpg");
 	background->setPosition(0, 0);
 	background->setAnchorPoint(Vec2(0, 0));
-	this->addChild(background,0,200);
+	this->addChild(background, 0, 200);
 	//background[1] = Sprite::create("background3.png");
-    //background[1]->setOpacity(150);
+	//background[1]->setOpacity(150);
 	//background[1]->setPosition(-600, -550);
 	//background[1]->setAnchorPoint(Vec2(0, 0));
 	//this->addChild(background[1],1 ,201);
@@ -48,6 +48,8 @@ bool HelloWorld::init()
 	addChild(hero);
 	monster = Monster::createMonsterSprite(Vec2(1050, 950), 2, "stand");
 	addChild(monster);
+	monster2 = Monster::createMonsterSprite(Vec2(320, 180), 4, "run");
+	addChild(monster2);
 	rocker = HRocker::createHRocker("rocker.png", "rockerBG.png", Vec2(100, 100));
 	addChild(rocker, 3);
 	rocker->startRocker(true);
@@ -75,7 +77,7 @@ void HelloWorld::update(float dt)
 				hero->setAction(hero->direction, "run", 8);
 			}
 		}
-		float r = sqrt((rocker->dx*rocker->dx) + (rocker->dy*rocker->dy));
+		float r = sqrt((rocker->dx * rocker->dx) + (rocker->dy * rocker->dy));
 		//CCLOG("hhh%f", r);
 		float x = 0;
 		float y = 0;
@@ -84,7 +86,7 @@ void HelloWorld::update(float dt)
 		{
 			//CCLOG("YES");
 		}
-		if (r !=0 )
+		if (r != 0)
 		{
 			float w = 1 / r;
 			float x = rocker->dx / r;
@@ -98,29 +100,31 @@ void HelloWorld::update(float dt)
 		float n = background->getPositionY();
 		float monsterX = monster->getPositionX();
 		float monsterY = monster->getPositionY();
-       // CCLOG("%f,%f", m, n);
-		/*auto pix = 320 - (int)m;
-		auto piy = 180 - (int)n;
-		//
-		if (x > 0)
-		{
-			pix = pix - 3;
-		}
-		else
-		{
-			pix = pix + 3;
-		}
-		if (y > 0)
-		{
-			piy += 3;
-		}
-		else
-		{
-			piy -= 3;
-		}*/
-		//if (getColor(pix, piy).a == 255)
-		auto pix = 320-(int)background->getPositionX();//320 - 
-		auto piy = 160-(int)background->getPositionY();//180 -
+		float monster2X = monster2->getPositionX();
+		float monster2Y = monster2->getPositionY();
+		// CCLOG("%f,%f", m, n);
+		 /*auto pix = 320 - (int)m;
+		 auto piy = 180 - (int)n;
+		 //
+		 if (x > 0)
+		 {
+			 pix = pix - 3;
+		 }
+		 else
+		 {
+			 pix = pix + 3;
+		 }
+		 if (y > 0)
+		 {
+			 piy += 3;
+		 }
+		 else
+		 {
+			 piy -= 3;
+		 }*/
+		 //if (getColor(pix, piy).a == 255)
+		auto pix = 320 - (int)background->getPositionX();//320 - 
+		auto piy = 160 - (int)background->getPositionY();//180 -
 		CCLOG("%d,%d", pix, piy);
 		/*if (x > 0)
 		{
@@ -141,21 +145,22 @@ void HelloWorld::update(float dt)
 		//if ((getColor(pix, piy).g ==255)&&(getColor(pix, piy).b == 0)&&(getColor(pix, piy).r == 0))
 		//if(getColor(pix, piy).g >=0)
 		//{
-			
+
 		   //auto v=getColor(0, 0);
 			//CCLOG("color r:%d g:%d b:%d a:%d ", v.r, v.g, v.b, v.a);
-			if (r != 0)
-			{
-				auto sp1 = this->getChildByTag(200);
-				//sp1->setPosition((m - x)*dt, (n - y)*dt);
-				//sp1->setPosition(m - x, n - y);
-				sp1->setPosition(m - rocker->dx / r, n - rocker->dy / r);
-				monster->setPosition(monsterX - rocker->dx / r, monsterY - rocker->dy / r);
-				//auto sp2 = this->getChildByTag(201);
-				//sp2->setPosition((m - x)*dt, (n - y)*dt);
-				//sp2->setPosition(m - rocker->dx / r, n - rocker->dy / r);
-			}
-	    //}
+		if (r != 0)
+		{
+			auto sp1 = this->getChildByTag(200);
+			//sp1->setPosition((m - x)*dt, (n - y)*dt);
+			//sp1->setPosition(m - x, n - y);
+			sp1->setPosition(m - rocker->dx / r, n - rocker->dy / r);
+			monster->setPosition(monsterX - rocker->dx / r, monsterY - rocker->dy / r);
+			monster2->setPosition(monster2X - rocker->dx / r, monster2Y - rocker->dy / r);
+			//auto sp2 = this->getChildByTag(201);
+			//sp2->setPosition((m - x)*dt, (n - y)*dt);
+			//sp2->setPosition(m - rocker->dx / r, n - rocker->dy / r);
+		}
+		//}
 	}
 	else if (hero->isRun == true && rocker->isCanMove == false)
 	{
@@ -171,6 +176,18 @@ void HelloWorld::update(float dt)
 	{
 		monster->waiting();
 	}
+	if (monster2->isAlive)
+	{
+		if (monster2->isAttacked)
+		{
+			monster2->runAttack(monster2);
+			monster2->isAttacked = 0;
+		}
+		else
+		{
+			monster2->setPosition(monster2->getPositionX()+0.5, monster2->getPositionY()+0.5);
+		}
+	}
 }
 Color4B HelloWorld::getColor(int x, int y)
 {
@@ -178,14 +195,14 @@ Color4B HelloWorld::getColor(int x, int y)
 	//image->initWithImageFile("background1.jpg");
 	y = image->getHeight() - y;
 	//y = 1690 - y;
-	unsigned char *m_pData = image->getData();
+	unsigned char* m_pData = image->getData();
 
 	//int x = (int)posx;
 	//int y = (int)posy;
 	Color4B c = { 0, 0, 0, 0 };
-	unsigned int *pixel = (unsigned int *)m_pData;
+	unsigned int* pixel = (unsigned int*)m_pData;
 	//int width = image->getWidth();
-	pixel = pixel + y*2998 + x;        //480 是图片的宽
+	pixel = pixel + y * 2998 + x;        //480 是图片的宽
 	c.r = *pixel & 0xff;
 	c.g = (*pixel >> 8) & 0xff;
 	c.b = (*pixel >> 16) & 0xff;
@@ -202,13 +219,10 @@ Color4B HelloWorld::getColor(int x, int y)
 	//libpng读取png图片时是从上到下逐行读取的，因此这里的纵坐标需要处理下。
 	y = image->getHeight() - y;
 	int width = image->getWidth();
-
 	unsigned char *data_ = image->getData();
 	unsigned int *pixel = (unsigned int *)data_;//以unsigned int来处理
-
 	pixel = pixel + (y * width) + x;//跳到指定的像素点。
 	c.a = (*pixel >> 24) & 0xff;
-
 	float scale = (c.a + 1.0f) / 256;
 	c.r = *pixel & 0xff;
 	c.r = c.r / scale;
