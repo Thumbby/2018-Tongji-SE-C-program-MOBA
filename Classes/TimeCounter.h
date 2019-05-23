88972a9d1b7420226eda0c17fe495d0788726778
yanyu <49723522+Yangyu0879@users.noreply.github.com>
2019-05-23 20:00:59 +0800

    Add files via upload

---
+++ Classes/TimeCounter.h
@@ -0,0 +1,14 @@
+#pragma once
+#include"cocos2d.h"
+USING_NS_CC;
+class TimeCounter :public Node
+{
+public:
+	CREATE_FUNC(TimeCounter);
+	virtual bool init();
+	virtual void update(float dt);
+	void start();//开始计时
+	float getfCurTime();
+private:
+	float m_mftime;
+};
